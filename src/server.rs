79df9a4035a8530813a9ff511b7ca26b9e9be142
Zach Roth <zach.roth21@gmail.com>
2023-06-07 17:04:58 -0700

    Reorganize Cargo.toml file

--- src/server.rs
+++
@@ -1,9 +0,0 @@
-use chat_lib::start_chat_server;
-
-/// Asynchronous main function that starts the chat server.
-///
-/// Calls `start_chat_server` function, internally handles error checking, doesn't return `Result`.
-#[tokio::main]
-async fn main() {
-    start_chat_server().await;
-}
